5fcee456a4bac413f8181654385e857ef2d5e5e1
inehadev <156919946+inehadev@users.noreply.github.com>
2024-06-10 09:36:01 +0530

    did some modification to the login api

--- Routes/Authentication/Authentication.js
+++ Routes/Authentication/Authentication.js
@@ -35,26 +35,27 @@ authRouter.post('/register' ,async (req,res)=>{
 authRouter.post('/login' , async(req , res)=>{
     try {
 
-        const {username , email ,password }=req.body;
-        const exisitinguser = await  user.findOne({username});
+        const { username, email ,password }=req.body;
+        const exisitinguser = await  user.findOne({username , email});
         if(!exisitinguser){
-            res.status(400).json({message:'user not found'})
+          return  res.status(400).json({message:'user not found'})
         }
-
+       
         const ismatch =  await bcrypt.compare(password , exisitinguser.password);
         if(!ismatch){
-            res.status(400).json({message:'password doesnot match'})
+            return res.status(400).json({message:'password doesnot match'})
         }
        
         const token =  jwt.sign({userId:exisitinguser._id} , "x-auth-token");
         if(token){
-            res.status(200).json({user:exisitinguser , token})
+          return  res.status(200).json({user:exisitinguser , token})
 
         }
-        
+       
+        return  res.status(500).json({ message: 'Failed to generate token' });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Server error' });
+        return  res.status(500).json({ message: 'Server error' });
     }
 })
 
