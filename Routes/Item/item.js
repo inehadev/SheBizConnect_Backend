a09f3d10b5783712443d9333e9ba91168f8052c2
inehadev <156919946+inehadev@users.noreply.github.com>
2024-07-13 22:49:38 +0530

    solved some error

--- Routes/Item/item.js
+++ Routes/Item/item.js
@@ -2,19 +2,24 @@ const express = require('express');
 const Item = require('../../models/ItemModel');
 const profile=require('../../models/ProfileModel');
 const protect = require('../../middleware/protect');
+const mongoose = require('mongoose');
 
 const ItemRoute = express.Router();
 
 ItemRoute.post('/createItem/:profileId' , protect , async(req,res)=>{
   try {
+    console.log("working fine")
       const {img  , price , name}=req.body;
+      const  { created_by}=req.user;
+   const  { profileId}=req.params;
     const existitem = await Item.findOne({name , img });
+    
+
     if(existitem){
         res.status(400).json("This item already exist");
         
     }
-   const  { created_by}=req.user;
-   const  { profileId}=req.params;
+   
    const Profile = await profile.findById(profileId);
 
     const newItem = new Item({
@@ -41,10 +46,28 @@ ItemRoute.post('/createItem/:profileId' , protect , async(req,res)=>{
  
     
   } catch (error) {
-    console.log(error);
-    res.json(error);
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error", error });
     
   }
 } )
 
+
+///api to get profileItem
+
+ItemRoute.get('/profileItem/:profileId' , protect , async(req,res)=>{
+  try {
+    const {profileId} =req.params;
+    const Profile = await profile.findById(profileId).populate('AddItem');
+    if(!Profile){
+      res.status(400).json("profile not found");
+    }
+    return res.status(200).json(Profile.AddItem);
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+    
+    
+  }
+})
 module.exports=ItemRoute;
